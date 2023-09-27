a2fa7c59860458b89dffadcc1782ac26131e7e62
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-26 20:23:09 -0700

    forgot to add the 'summary paragraph' at the end of the function - tweaks made to accomodate the new inclusion

--- survey.js
+++ survey.js
@@ -7,35 +7,33 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// What's your name? Do you have a nickname?
-rl.question("What's your name? Do you have a nickname? ", (answer1) => {
-  console.log(`Your answer was: ${answer1}`);
-  
-  // What's an activity you like doing?
-  rl.question("What's an activity you like doing? ", (answer2) => {
-    console.log(`Your answer was: ${answer2}`);
-    
-    // What do you listen to while doing that (activity)?
-    rl.question("What do you listen to while doing that activity?", (answer3) => {
-      console.log(`Your answer was: ${answer3}`);
-      
-      // Which meal is your favourite (eg: dinner, brunch, midnight snack, etc.)
-      rl.question("Which meal is your favourite? (eg: dinner, brunch, midnight snack, etc.)", (answer4) => {
-        console.log(`Your answer was: ${answer4}`);
-        
-        // What's your favourite thing to eat for that perticular meal?
-        rl.question("What's your favourite thing to eat for that particular meal?", (answer5) => {
-          console.log(`Your answer was: ${answer5}`);
-          
-          // which sport is your absolute favourite? Which team/individual do you support/follow in that sport?
-          rl.question("Which sport is your absolute favourite sport? Which team/individual do you support/follow in that sport?", (answer6) => {
-            console.log(`Your answer was: ${answer6}`);
+let answers = [];
 
-            // What is your superpower? In a few words, tell us what you are amazing at!
-            rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (answer7) => {
-              console.log(`Your answer was: ${answer7}`);
+rl.question("What's your name? ", (answer1) => {
+  answers.push(answer1); // edited the call from console.log to a function push
+
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    answers.push(answer2);
+    
+    rl.question("What do you listen to while doing that activity? ", (answer3) => {
+      answers.push(answer3);
+      
+      rl.question("Which meal is your favourite? (eg: dinner, brunch, midnight snack, etc.) ", (answer4) => {
+        answers.push(answer4);
+        
+        rl.question("What's your favourite thing to eat for that particular meal? ", (answer5) => {
+          answers.push(answer5);
+          
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            answers.push(answer6);
+
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+              answers.push(answer7);
               
               rl.close();
+
+              const sumPara = `${answer1}, loves to listen to ${answer3} while they are ${answer2}.\n${answer1} also loves consuming ${answer5} for ${answer4}.\nThey believe ${answer6} is the superior sport. Most importantly, they are ${answer7}.` 
+              console.log(sumPara);
             });
           });
         });
